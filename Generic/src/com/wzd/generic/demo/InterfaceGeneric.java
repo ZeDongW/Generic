34b89c92feeb12c9bb8dc1b9487cf247e4e11a05
ZeDongW <42669314+ZeDongW@users.noreply.github.com>
2018-12-27 00:14:56 +0800

    接口上自定义泛型

---
+++ Generic/src/com/wzd/generic/demo/InterfaceGeneric.java
@@ -0,0 +1,20 @@
+/**  
+* @Title: InterfaceGeneric.java  
+* @Package com.wzd.generic.demo  
+* @Description: TODO(用一句话描述该文件做什么)  
+* @author wangzedong  
+* @date 2018年12月26日下午11:49:21  
+* @version V1.0  
+*/    
+package com.wzd.generic.demo;  
+  
+/**  
+* @ClassName: InterfaceGeneric  
+* @Description: TODO(这里用一句话描述这个类的作用)  
+* @author wangzedong  
+* @date 2018年12月26日下午11:49:21  
+*    
+*/
+public interface InterfaceGeneric<T> {
+    public void add(T t);
+}
---
+++ Generic/src/com/wzd/generic/demo/InterfaceGenericImpl.java
@@ -0,0 +1,32 @@
+/**  
+* @Title: InterfaceGenericImpl.java  
+* @Package com.wzd.generic.demo  
+* @Description: TODO(用一句话描述该文件做什么)  
+* @author wangzedong  
+* @date 2018年12月26日下午11:50:50  
+* @version V1.0  
+*/    
+package com.wzd.generic.demo;  
+  
+/**  
+* @ClassName: InterfaceGenericImpl  
+* @Description: TODO(这里用一句话描述这个类的作用)  
+* @author wangzedong  
+* @date 2018年12月26日下午11:50:50  
+*    
+*/
+public class InterfaceGenericImpl<T> implements InterfaceGeneric<T> {
+
+    /**
+    * <p>Title: add</p>  
+    * <p>Description: </p>  
+    * @param t  
+    * @see com.wzd.generic.demo.InterfaceGeneric#add(java.lang.Object)  
+    */  
+    @Override
+    public void add(T t) {
+        // TODO Auto-generated method stub  
+        
+    }
+
+}
